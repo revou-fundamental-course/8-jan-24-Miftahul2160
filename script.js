9e149ceb5dcaf1768efae31dedfc34f065a250a2
Miftahul2160 <157046372+Miftahul2160@users.noreply.github.com>
2024-01-18 23:47:47 +0700

    Create script.js
    File hanya berupa javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+function main() {
+    var sisi;
+
+    window.alert("Rumus Keliling Persegi : 4 x Sisi");
+    window.alert("Rumus Luas Persegi : Sisi  x Sisi");
+    window.alert("Berapa panjang sisi persegi?");
+    sisi = Number(window.prompt('Enter a value for sisi'));
+    window.alert("Jadi keliling persegi dari panjang sisi " + sisi + " adalah = " + 4 * sisi);
+    window.alert("Jadi luas persegi dari panjang sisi " + sisi + " adalah = " + sisi * sisi);
+}
